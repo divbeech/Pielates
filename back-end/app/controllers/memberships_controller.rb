236572edcb1218f0eb038fc5601911c1c5fa9209
divbeech <dyadav1006@gmail.com>
2020-04-28 15:16:41 -0400

    update

--- back-end/app/controllers/memberships_controller.rb
+++ back-end/app/controllers/memberships_controller.rb
@@ -2,7 +2,6 @@ class MembershipsController < ApplicationController
     skip_before_action :verify_authenticity_token
 
     def create
-        # byebug
         membership = Membership.create(user_id: params[:user_id], name: params[:name], yelp_id: params[:yelp_id], image: params[:image], address: params[:address], phone: params[:phone])
 
        memberships = Membership.all
