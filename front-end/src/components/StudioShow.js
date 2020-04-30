f843fbc1738d63bb752bfe76473f397d21d50b30
divbeech <dyadav1006@gmail.com>
2020-04-30 12:09:49 -0400

    showpage

--- front-end/src/components/StudioShow.js
+++ front-end/src/components/StudioShow.js
@@ -4,17 +4,17 @@ const StudioShow = (props) => {
     return (
         <div className="polaroid">
           {console.log(props)}
-          {/* <h1>{props.studio.name}<br>
-            <p></p>{props.studio.address}<br>
-            {props.studio.price}<br>
-            {props.studio.rating} </p> </h1>
-       
-          <img src={this.props.studio.image_url} />
+          <h1>{props.location.state.current_studio.name}<br/>
+            <p>{props.location.state.current_studio.address}<br/>
+            {props.location.state.current_studio.price}<br/>
+            {props.location.state.current_studio.rating} </p> 
+            {props.location.state.current_studio.reviews}</h1>
+          <img src={props.location.state.current_studio.image_url} />
           <div className="imagecontainer">
-          <p>{this.props.studio.name}</p> <br />
-          <p>{this.props.studio.address}</p> </div> */}
+          </div>
         </div>
       );
 }
 
 export default StudioShow;
+
